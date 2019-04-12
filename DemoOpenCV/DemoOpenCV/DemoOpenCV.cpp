7617a9138f5f209dc2c0b7db78068696308d7414
Nicolas Lee <312725531@qq.com>
2019-04-12 10:04:42 +0800

    清理DemoOpencv

--- DemoOpenCV/DemoOpenCV/DemoOpenCV.cpp
+++ DemoOpenCV/DemoOpenCV/DemoOpenCV.cpp
@@ -3,11 +3,19 @@
 
 #include "stdafx.h"
 
-#include <cv.hpp>
-#include <imgproc.hpp>
+#include <iostream>
+#include "opencv2/highgui/highgui.hpp"
 
 int _tmain(char agrc, char** agrv)
 {
-	cv::pyrUp();
+	cv::Mat src = cv::imread("C:\\Users\\simpl\\Desktop\\Temp\\3 generation 20 pcs\\1.bmp", -1);
+	if (src.empty())
+	{
+		std::cout << "read image failed." << std::endl;
+	}
+	cv::namedWindow("src", cv::WINDOW_NORMAL);
+	cv::imshow("src", src);
+	cv::waitKey(0);
+	cv::destroyWindow("src");
 	return 0;
 }
